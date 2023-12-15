27c1f57b6a9b8c4ae0f97c1baa50c774d381c7f1
ANSN7 <alkanixon97@gmail.com>
2023-12-15 16:06:26 +0000

    Added resource input and sample resource type form.

--- app/src/routes/(time)/form/TutorsResourceForm.svelte
+++ app/src/routes/(time)/form/TutorsResourceForm.svelte
@@ -1,34 +1,44 @@
 <script lang="ts">
+  import TutorsResourceTypeForm from "./TutorsResourceTypeForm.svelte";
 
-type Unit = {
-      id: number;
-      title: string;
-      type: string;
-      resources: [];
-    };
-    
-type Topic = {
+  type Unit = {
+    id: number;
+    title: string;
+    type: string;
+    resources: [];
+  };
+
+  type Topic = {
     id: number;
     title: string;
     desc: string;
     icon: string;
     units: Unit[];
   };
-  
-export let formData: {
-      courseName: string;
-      courseDescription: string;
-      topics: Topic[];
-      topicName: string;
-      unitName: string;
-      resourceName: string;
-  };
-  
-  </script>
 
-  <div class="flex mt-4 space-x-4">
-    <div class="flex flex-col">
-      <label for="topicName">Resource Name:</label>
-      <input type="text" id="topicName" bind:value={formData.resourceName} class="border rounded p-2" />
-    </div>
-  </div>
+  export let formData: {
+    courseName: string;
+    courseDescription: string;
+    topics: Topic[];
+    topicName: string;
+    unitName: string;
+    resourceName: string;
+  };
+
+  let resources: String[] = ["Web", "Github", "Archive", "Lab", "Presentation", "Note", "Video"];
+</script>
+
+<div class="flex flex-col items-left">
+  <label for="resourceName" class="mb-2">Resource:</label>
+  <select id="resourceName" class="border rounded p-2 mb-2 w-64" bind:value={formData.resourceName}>
+    <option value="" selected>Select resource</option>
+    {#each resources as resource}
+      <option value={resource}>
+        {resource}
+      </option>
+    {/each}
+  </select>
+  {#if formData.resourceName}
+    <TutorsResourceTypeForm {formData} />
+  {/if}
+</div>
---
+++ app/src/routes/(time)/form/TutorsResourceTypeForm.svelte
@@ -0,0 +1,30 @@
+<form class="w-full max-w-lg pt-4">
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="web-title"> Enter Title </label>
+      <input
+        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+        id="web-title"
+        type="text"
+      />
+    </div>
+    <div class="w-full md:w-1/2 px-3">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="web-description"> Enter Description </label>
+      <input
+        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+        id="web-description"
+        type="text"
+      />
+    </div>
+  </div>
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full px-3">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="url"> Enter URL </label>
+      <input
+        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+        id="url"
+        type="url"
+      />
+    </div>
+  </div>
+</form>
